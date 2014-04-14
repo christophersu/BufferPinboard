b168bbaacce9bb64f89c343774a57421750ae24e
Christopher Su <christophersu9@gmail.com>
2014-04-14 14:24:52 -0700

    it works! and it automatically shortens the links. only concern: string encoding

--- pinboard-to-buffer.py
+++ pinboard-to-buffer.py
@@ -7,7 +7,9 @@ p = pinboard.open(token=PinboardCredentials.PINBOARD_API_TOKEN)
 # posts = p.posts(fromdt=p["last_updated"]) # only get content since last run
 pastdays = date.today() - timedelta(3)
 posts = p.posts(fromdt=pastdays)
-print posts
+updates = []
+for post in posts:
+	updates.append(post["description"] + ' ' + post["href"])
 
 from pprint import pprint as pp
 from colorama import Fore
@@ -17,10 +19,10 @@ from buffpy.managers.updates import Updates
 from buffpy.api import API
 
 token = BufferCredentials.BUFFER_APP_TOKEN
-
 api = API(client_id=BufferCredentials.BUFFER_CLIENT_ID,
           client_secret=BufferCredentials.BUFFER_CLIENT_SECRET,
           access_token=token)
-
 profile = Profiles(api=api).filter(service='twitter')[0]
-print profile.updates.pending
+
+for update in updates:
+	profile.updates.new(update.encode('ascii', 'ignore'))
